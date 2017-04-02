b98f6e34a6ba6450f533a4823395a0496510826b
Hayden Oster <hayden94@gmail.com>
2017-04-02 12:47:49 -0700

    chore: Cleaned code
    The code has been cleaned up by removing a large portion of the .hide()
    and .show() methods and replacing them with one.

--- js/script.js
+++ js/script.js
@@ -1,58 +1,38 @@
 var count1, count2, count3, count4, count5;
 count1 = count2 = count3 = count4 = count5 = 0;
 
-$("#catPics").hide();
+$(".pics").hide();
 
 $("#link1").click(function () {
-    $("#catPics").show();
-    $("#pic2").hide();
-    $("#pic3").hide();
-    $("#pic4").hide();
-    $("#pic5").hide();
+    $(".pics").hide();
     $('#name').text('Cherry');
     $('#counter').text(count1);
     $("#pic1").show();
 });
 
 $("#link2").click(function () {
-    $("#catPics").show();
-    $("#pic1").hide();
-    $("#pic3").hide();
-    $("#pic4").hide();
-    $("#pic5").hide();
+    $(".pics").hide();
     $('#name').text('Anabell');
     $('#counter').text(count2);
     $("#pic2").show();
 });
 
 $("#link3").click(function () {
-    $("#catPics").show();
-    $("#pic1").hide();
-    $("#pic2").hide();
-    $("#pic4").hide();
-    $("#pic5").hide();
+    $(".pics").hide();
     $('#name').text('Fox');
     $('#counter').text(count3);
     $("#pic3").show();
 });
 
 $("#link4").click(function () {
-    $("#catPics").show();
-    $("#pic1").hide();
-    $("#pic2").hide();
-    $("#pic3").hide();
-    $("#pic5").hide();
+    $(".pics").hide();
     $('#name').text('Goobs');
     $('#counter').text(count4);
     $("#pic4").show();
 });
 
 $("#link5").click(function () {
-    $("#catPics").show();
-    $("#pic1").hide();
-    $("#pic2").hide();
-    $("#pic3").hide();
-    $("#pic4").hide();
+    $(".pics").hide();
     $('#name').text('Wrinkles');
     $('#counter').text(count5);
     $("#pic5").show();
