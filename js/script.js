8bfc92456ec97abd39100666cd3d23120f4cb5fd
Hayden Oster <hayden94@gmail.com>
2017-04-06 18:42:23 -0700

    feat: Admin button
    A much long overdue commit. Model, Octopus, and View(MOV) refactoring
    and design implementations. Users now have the ability to change the
    cat's name, image, and click count number through the use of the admin
    button.

--- js/script.js
+++ js/script.js
@@ -1,64 +1,225 @@
-var count1, count2, count3, count4, count5;
-count1 = count2 = count3 = count4 = count5 = 0;
+var model = {
+    currentCat: null,
+    showAdmin: false,
+    cats: [
+        {
+            clickCount: 0,
+            name: 'Cherry',
+            imgSrc: 'https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg'
+        },
+        {
+            clickCount: 0,
+            name: 'Anabell',
+            imgSrc: 'https://s-media-cache-ak0.pinimg.com/736x/92/9d/3d/929d3d9f76f406b5ac6020323d2d32dc.jpg'
+        },
+        {
+            clickCount: 0,
+            name: 'Fox',
+            imgSrc: 'https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-06.jpg',
+        },
+        {
+            clickCount: 0,
+            name: 'Goobs',
+            imgSrc: 'https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg'
+        },
+        {
+            clickCount: 0,
+            name: 'Wrinkles',
+            imgSrc: 'http://www.91x.com/wp-content/uploads/2016/12/Cat.jpg'
+        }
+    ]
+};
 
-$(".pics").hide();
+var octopus = {
 
-$("#link1").click(function () {
-    $(".pics").hide();
-    $('#name').text('Cherry');
-    $('#counter').text(count1);
-    $("#pic1").show();
-});
+    init: function() {
+        // set our current cat to the first one in the list
+        model.currentCat = model.cats[0];
 
-$("#link2").click(function () {
-    $(".pics").hide();
-    $('#name').text('Anabell');
-    $('#counter').text(count2);
-    $("#pic2").show();
-});
+        // tell our views to initialize
+        catListView.init();
+        catView.init();
+        adminView.init();
+    },
 
-$("#link3").click(function () {
-    $(".pics").hide();
-    $('#name').text('Fox');
-    $('#counter').text(count3);
-    $("#pic3").show();
-});
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
 
-$("#link4").click(function () {
-    $(".pics").hide();
-    $('#name').text('Goobs');
-    $('#counter').text(count4);
-    $("#pic4").show();
-});
+    getCats: function() {
+        return model.cats;
+    },
 
-$("#link5").click(function () {
-    $(".pics").hide();
-    $('#name').text('Wrinkles');
-    $('#counter').text(count5);
-    $("#pic5").show();
-});
+    getAdmin: function() {
+        return model.showAdmin;
+    },
 
-$('#pic1').click(function (e) {
-    count1 += 1;
-    $('#counter').text(count1);
-});
+    openAdmin: function() {
+        model.showAdmin = true;
+    },
 
-$('#pic2').click(function (e) {
-    count2 += 1;
-    $('#counter').text(count2);
-});
+    closeAdmin: function() {
+        model.showAdmin = false;
+    },
 
-$('#pic3').click(function (e) {
-    count3 += 1;
-    $('#counter').text(count3);
-});
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
 
-$('#pic4').click(function (e) {
-    count4 += 1;
-    $('#counter').text(count4);
-});
+    // increments the counter for the currently-selected cat
+    incrementCounter: function() {
+        model.currentCat.clickCount++;
+        catView.render();
+    }
+};
 
-$('#pic5').click(function (e) {
-    count5 += 1;
-    $('#counter').text(count5);
-});
+
+/* ======= View ======= */
+
+var catView = {
+
+    init: function() {
+        // store pointers to our DOM elements for easy access later
+        this.catElem = document.getElementById('cat');
+        this.catNameElem = document.getElementById('cat-name');
+        this.catImageElem = document.getElementById('cat-img');
+        this.countElem = document.getElementById('cat-count');
+
+        // on click, increment the current cat's counter
+        this.catImageElem.addEventListener('click', function(){
+            octopus.incrementCounter();
+        });
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+
+        adminFormElem = document.getElementById('btn-content');
+        octopus.closeAdmin();
+        adminFormElem.style.visibility = 'hidden';
+
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.imgSrc;
+    }
+};
+
+var catListView = {
+
+    init: function() {
+        // store the DOM element for easy access later
+        this.catListElem = document.getElementById('cat-list');
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        var cat, elem, i;
+        // get the cats we'll be rendering from the octopus
+        var cats = octopus.getCats();
+
+        // empty the cat list
+        this.catListElem.innerHTML = '';
+
+        // loop over the cats
+        for (i = 0; i < cats.length; i++) {
+            // this is the cat we're currently looping over
+            cat = cats[i];
+
+            // make a new cat list item and set its text
+            elem = document.createElement('li');
+            elem.textContent = cat.name;
+
+            // on click, setCurrentCat and render the catView
+            // (this uses our closure-in-a-loop trick to connect the value
+            //  of the cat variable to the click event function)
+            elem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
+                };
+            })(cat));
+
+            // finally, add the element to the list
+            this.catListElem.appendChild(elem);
+        }
+    }
+};
+
+var adminView = {
+
+    init: function() {
+        this.adminBtnElem = document.getElementById('btn-admin');
+        this.adminFormElem = document.getElementById('btn-content');
+        this.adminCancelElem = document.getElementById('btn-cancel');
+        this.adminSaveElem = document.getElementById('btn-save');
+
+        this.adminFormElem.style.visibility = 'hidden';
+
+        this.render();
+    },
+
+    render: function() {
+        adminNameElem = document.getElementById('btn-name');
+        adminImgElem = document.getElementById('btn-img');
+        adminCountElem = document.getElementById('btn-count');
+
+        // When admin button is clicked, decide whether to open or close the form
+        this.adminBtnElem.addEventListener('click', function() {
+            if (octopus.getAdmin() == false) {
+                var currentCat = octopus.getCurrentCat();
+                octopus.openAdmin();
+                adminFormElem.style.visibility = 'visible';
+
+                // set placeholder values to currentCat
+                adminNameElem.placeholder = currentCat.name;
+                adminImgElem.placeholder = currentCat.imgSrc;
+                adminCountElem.placeholder = currentCat.clickCount;
+            } else {
+                octopus.closeAdmin();
+                adminFormElem.style.visibility = 'hidden';
+            }
+        });
+
+        // When cancel button is clicked, close form and set value to false.
+        this.adminCancelElem.addEventListener('click', function() {
+            octopus.closeAdmin();
+            adminFormElem.style.visibility = 'hidden';
+        });
+
+        // When Save element is clicked, get input values, set them to currentCat, and render new values
+        this.adminSaveElem.addEventListener('click', function() {
+            // Get current cat to store new values
+            var currentCat = octopus.getCurrentCat();
+
+            // Set currentCat values to admin input values.
+            currentCat.name = adminNameElem.value;
+            currentCat.imgSrc = adminImgElem.value;
+            currentCat.clickCount = adminCountElem.value;
+
+            console.log(adminNameElem.value);
+            console.log(adminImgElem.value);
+            console.log(adminCountElem.value);
+
+            // close form elem, set value to false, and render new values
+            octopus.closeAdmin();
+            catView.render();
+            catListView.render();
+            // make form values empty for next Admin click
+            adminNameElem.value = '';
+            adminImgElem.value = '';
+            adminCountElem.value = '';
+
+            adminFormElem.style.visibility = 'hidden';
+        });
+    }
+};
+
+// make it go!
+octopus.init();
